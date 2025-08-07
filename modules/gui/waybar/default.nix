62355e687298bf5b580ed4c613d8872d57faf70a
clement fombonne <clement.fombonne@proton.me>
2025-08-07 18:28:14 +0200

    add nwgbar

--- modules/gui/waybar/default.nix
+++ modules/gui/waybar/default.nix
@@ -6,8 +6,11 @@ let cfg = config.modules.waybar;
 in {
     options.modules.waybar = { enable = mkEnableOption "waybar"; };
     config = mkIf cfg.enable {
+        environment.systemPackages = with pkgs; [
+            nwg-bar
+        ];
         fonts.packages = with pkgs; [
             font-awesome
-	];
+	    ];
     };
 }
--- modules/gui/waybar/home.nix
+++ modules/gui/waybar/home.nix
@@ -7,8 +7,17 @@ in {
     config = mkIf cfg.enable {
         programs.waybar.enable = true;
 
+        # waybar config  
         home.file.".config/waybar/config.jsonc".source = ./config.jsonc;
         home.file.".config/waybar/style.css".source = ./style.css;
         home.file.".config/waybar/frappe.css".source = ./frappe.css;
+
+        # nwg-bar
+        home.file.".config/nwg-bar/bar.json".source = ./nwg-bar/bar.json;
+        home.file.".config/nwg-bar/style.css".source = ./nwg-bar/style.css;
+        home.file.".config/nwg-bar/images/system-lock-screen.svg".source = ./nwg-bar/system-lock-screen.svg;
+        home.file.".config/nwg-bar/images/system-reboot.svg".source = ./nwg-bar/system-reboot.svg;
+        home.file.".config/nwg-bar/images/system-log-out.svg".source = ./nwg-bar/system-log-out.svg;
+        home.file.".config/nwg-bar/images/system-shutdown.svg".source = ./nwg-bar/system-shutdown.svg;
     };
 }
