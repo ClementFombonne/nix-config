abce7f8fdcf786f525babc5911391bef046df22c
clement fombonne <clement.fombonne@proton.me>
2025-08-08 22:01:51 +0200

    draft sddm theme

--- modules/gui/sddm/default.nix
+++ modules/gui/sddm/default.nix
@@ -7,19 +7,20 @@ in {
     options.modules.sddm = { enable = mkEnableOption "sddm"; };
     config = mkIf cfg.enable {
         environment.systemPackages = with pkgs; [(
-	    catppuccin-sddm.override {
-                flavor = "frappe";
-                font  = "Noto Sans";
-                fontSize = "12";
-                background = "${../../../assets/wallpaper.jpg}";
-                loginBackground = true;
+            sddm-astronaut.override {
+                themeConfig = {
+                    Background = "/home/clement/.config/wallpaper/glass.png";
+                    ScreenWidth = "3072";
+                    ScreenHeight = "1920";
+                };
             }
-	)];
+	    )];
         services.xserver.enable = true;
         services.displayManager.sddm = {
             enable = true;
-            theme = "catppuccin-frappe";
-	    package = pkgs.kdePackages.sddm;
+            theme = "sddm-astronaut-theme";
+	        package = pkgs.kdePackages.sddm;
+            extraPackages = [ pkgs.sddm-astronaut ];
         };
     };
 }
