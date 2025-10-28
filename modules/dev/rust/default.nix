ddc9f44182950a24a923898d46e37ec92e420690
clement fombonne <clement.fombonne@proton.me>
2025-10-28 15:44:51 +0100

    update rust

--- modules/dev/rust/default.nix
+++ modules/dev/rust/default.nix
@@ -16,7 +16,8 @@ in
   };
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      rust-bin.stable.latest
+      rustc
+      cargo
       rust-analyzer
       rustup
       clippy
