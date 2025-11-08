8e9314fe44cd643be0c42167a17c93c5c85ec6e9
clement fombonne <clement.fombonne@proton.me>
2025-11-08 15:10:08 +0100

    update fastfetch

--- modules/cli/packages/default.nix
+++ modules/cli/packages/default.nix
@@ -1,29 +1,37 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 with lib;
-let cfg = 
-    config.modules.packages;
+let
+  cfg = config.modules.packages;
 
-in {
-    options.modules.packages = { enable = mkEnableOption "packages"; };
-    config = mkIf cfg.enable {
-        environment.systemPackages = with pkgs; [
-	    ffmpeg 
-	    tealdeer
-	    htop 
-	    fzf
-	    fastfetch
-	    eza
-            bat 
-	    unzip 
-	    pqiv
-            wf-recorder 
-	];
-	environment.shellAliases = {
-            ls = "eza --icons=auto";
-	    ll = "eza --icons=auto -l --git --total-size";
-	    l =  "eza --icons=auto -la";
-	    tree = "eza --icons=auto --tree";
-	};
+in
+{
+  options.modules.packages = {
+    enable = mkEnableOption "packages";
+  };
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      ffmpeg
+      tealdeer
+      htop
+      fzf
+      fastfetch
+      eza
+      bat
+      unzip
+      pqiv
+      wf-recorder
+    ];
+    environment.shellAliases = {
+      ls = "eza --icons=auto";
+      ll = "eza --icons=auto -l --git --total-size";
+      l = "eza --icons=auto -la";
+      tree = "eza --icons=auto --tree";
     };
+  };
 }
--- modules/cli/packages/home.nix
+++ modules/cli/packages/home.nix
@@ -1,16 +1,24 @@
-{ pkgs, lib, config, osConfig, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  osConfig,
+  ...
+}:
 
 with lib;
-let cfg = 
-    osConfig.modules.packages;
+let
+  cfg = osConfig.modules.packages;
 
-in {
-    config = mkIf cfg.enable {
-        programs.zsh.shellGlobalAliases = {
-            ls = "eza --icons=auto";
-	    ll = "eza --icons=auto -l --git --total-size";
-	    l =  "eza --icons=auto -la";
-	    tree = "eza --icons=auto --tree";
-	};
+in
+{
+  config = mkIf cfg.enable {
+    programs.zsh.shellGlobalAliases = {
+      ls = "eza --icons=auto";
+      ll = "eza --icons=auto -l --git --total-size";
+      l = "eza --icons=auto -la";
+      tree = "eza --icons=auto --tree";
     };
+  };
+  home.file.".config/fastfetch/config.jsonc".source = ./config.jsonc;
 }
