95c82e8a7a7cc474898b26f62ec587534ec3bb6b
clement fombonne <clement.fombonne@proton.me>
2025-12-01 16:18:35 +0100

    remove xdg

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -121,8 +121,6 @@
   environment.variables = {
     NIXOS_CONFIG = "$HOME/.config/nixos/modules/configuration.nix";
     NIXOS_CONFIG_DIR = "$HOME/.config/nixos/";
-    XDG_DATA_HOME = "$HOME/.local/share";
-    XDG_CONFIG_HOME = "$HOME/.config/";
     PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
     GTK_RC_FILES = "$HOME/.local/share/gtk-1.0/gtkrc";
     GTK2_RC_FILES = "$HOME/.local/share/gtk-2.0/gtkrc";
